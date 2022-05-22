7188ae8573c810b4a27127bb1881f57f75989988
Hamza Zaidi <humza.zaidi@gmail.com>
2022-05-22 15:40:15 -0400

    add component and add validation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit {  
+  searchLabel: string = 'Search Block/ Tx / Account';
+  apis: string[] = ['Mainnet', 'LocalTerra'];
+  currencies: string[] = [
+    'AUD',
+    'CAD',
+    'CHF',
+    'CNY',
+    'DKK',
+    'EUR',
+    'GBP',
+    'HKD',
+    'IDR',
+    'INR',
+    'JPY',
+    'KRW',
+    'MNT',
+    'MYR',
+    'NOK',
+    'PHP',
+    'SDR',
+    'SEK',
+    'SGD',
+    'THB',
+    'TWD',
+    'USD',
+  ];  
   constructor() { }
-
-  ngOnInit(): void {
-  }
+  form = new FormGroup({
+    api: new FormControl('Mainnet'),
+    currency: new FormControl('USD'),
+    search: new FormControl('', Validators.required)
+  });  
+  ngOnInit(): void {}
+  onSearch() {
+    const {search} = this.form.getRawValue()    
+   }
 
 }
